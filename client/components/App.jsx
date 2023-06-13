c1ff96e7bba9b6e0ee59129de884f7f54218d12e
erikahjung <erika.h.jung@gmail.com>
2023-06-12 17:57:26 -0700

    basic ui outline but calcuation logic is buggy

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Display } from './Display.jsx';
+import { Button } from './Button.jsx';
 
 const App = () => {
+  const [display, setDisplay] = useState('0');
+  const [calculation, setCalculation] = useState('');
+
+  const calculate = () => {
+    try {
+      const total = eval(calculation);
+      setDisplay(String(total));
+      setCalculation(String(total));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const onClick = (e) => {
+    switch (e.target.value) {
+      case '+': 
+        setCalculation((x) => x + '+');
+        break;
+      case '-':
+        setCalculation((x) => x + '-');
+        break;
+      case '/':
+        setCalculation((x) => x + '/');
+        break;
+      case '*':
+        setCalculation((x) => x + '*');
+        break;
+      case '=':
+        calculate();
+        setCalculation('');
+        break;
+      case 'C':
+        setDisplay('0');
+        setCalculation('');
+        break;
+      default:
+        setDisplay(e.target.value);
+        setCalculation((x) => x + e.target.value);
+    }
+    console.log(calculation);
+  }
+
   return (
-    <h1>My Calculator</h1>
+    <>
+      <h1>My Calculator</h1>
+      <Display number={display}></Display>
+      <Button symbol='1' onClick={onClick}></Button>
+      <Button symbol='2' onClick={onClick}></Button>
+      <Button symbol='3' onClick={onClick}></Button>
+      <Button symbol='4' onClick={onClick}></Button>
+      <Button symbol='5' onClick={onClick}></Button>
+      <Button symbol='6' onClick={onClick}></Button>
+      <Button symbol='7' onClick={onClick}></Button>
+      <Button symbol='8' onClick={onClick}></Button>
+      <Button symbol='9' onClick={onClick}></Button>
+      <Button symbol='0' onClick={onClick}></Button>
+      <Button symbol='+' onClick={onClick}></Button>
+      <Button symbol='-' onClick={onClick}></Button>
+      <Button symbol='/' onClick={onClick}></Button>
+      <Button symbol='*' onClick={onClick}></Button>
+      <Button symbol='=' onClick={onClick}></Button>
+      <Button symbol='C' onClick={onClick}></Button>
+    </>
   )
 }
 
