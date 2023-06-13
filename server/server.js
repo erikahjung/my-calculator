e050f60e6e81353e12859b8c19a40ab4e685be86
erikahjung <erika.h.jung@gmail.com>
2023-06-12 17:03:36 -0700

    boilerplate code

--- server/server.js
+++ server/server.js
@@ -24,6 +24,11 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
-    
+    status: 400,
+    message: { err: 'An error occurred' }
   };
+  const errorObj = Object.assign({}, defaultErr, err);
+  return res.status(errorObj.status).json(errorObj.message);
 })
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}...`));
